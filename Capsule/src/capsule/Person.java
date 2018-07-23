a95d70155b7815e0c6010fb7298859338c24cba6
masunaritatsuya <smsk.tatsu@gmail.com>
2018-07-23 21:01:49 +0900

    7月23日

--- Capsule/src/capsule/Person.java
+++ Capsule/src/capsule/Person.java
@@ -5,4 +5,19 @@ public class Person {
 	private String name=null;
 	private int age=0;
 
+	public String getName(){
+		return this.name;
+	}
+	public int getAge(){
+		return this.age;
+	}
+
+	public void setName(String name){
+		this.name=name;
+	}
+
+	public void setAge(int age){
+		this.age=age;
+	}
+
 }
