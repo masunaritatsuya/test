d638aec5f5e310a1a059c4e428334cbf543d06ff
sachiyuki <sachiyuki@outlook.jp>
2018-07-30 21:40:28 +0900

    7月30日

--- login/sql/struts.sql
+++ login/sql/struts.sql
@@ -10,5 +10,4 @@ user_name varchar(255),
 password varchar(255)
 );
 
-insert into user values
-(1,"taro","123"),(2,"jiro","123"),(3,"hanako","123"),(4,"saburo","123");
+insert into user values(1,"taro","123"),(2,"jiro","123"),(3,"hanako","123"),(4,"saburo","123");
