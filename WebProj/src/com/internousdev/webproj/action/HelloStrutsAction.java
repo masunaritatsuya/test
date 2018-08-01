1434fa69e206c18115b3abb3444469e86a93b1f5
shohei-izaki <izaki.yb1998@gmail.com>
2018-08-01 21:38:29 +0900

    8月1日

--- WebProj/src/com/internousdev/webproj/action/HelloStrutsAction.java
+++ WebProj/src/com/internousdev/webproj/action/HelloStrutsAction.java
@@ -1,6 +1,6 @@
 package com.internousdev.webproj.action;
 
-import com.opensymphony.xwork2.ActionSupport:
+import com.opensymphony.xwork2.ActionSupport;
 
 public class HelloStrutsAction extends ActionSupport{
 
--- WebProj/src/com/internousdev/webproj/action/WelcomeAction.java
+++ WebProj/src/com/internousdev/webproj/action/WelcomeAction.java
@@ -3,7 +3,7 @@ package com.internousdev.webproj.action;
 import com.opensymphony.xwork2.ActionSupport;
 
 
-public class WelcomeAction etends ActionSupport{
+public class WelcomeAction extends ActionSupport{
 
 	public String execute() {
 		return SUCCESS;
