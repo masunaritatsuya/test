112377fbd7d608143d776f3d2eef9920a82b113f
masunaritatsuya <smsk.tatsu@gmail.com>
2018-08-03 21:58:50 +0900

    8月3日

---
+++ WebProj/src/com/internousdev/webproj/action/TestAction.java
@@ -0,0 +1,20 @@
+package com.internousdev.webproj.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport{
+
+	private String username;
+	
+	private String password;
+	
+	public String execute()
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
